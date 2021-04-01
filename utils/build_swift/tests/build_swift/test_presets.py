89da66227b3bd3882392b29b51da4aff52ae078f
Anton Pogonets <apogonets@readdle.com>
2021-04-01 15:57:54 +0300

    Fix spelling arm_dir -> icu_dir

--- utils/build_swift/tests/build_swift/test_presets.py
+++ utils/build_swift/tests/build_swift/test_presets.py
@@ -46,7 +46,7 @@ PRESET_DEFAULTS = {
     'swift_install_destdir': '/tmp/install/swift',
     'symbols_package': '/path/to/symbols/package',
     'ndk_path': '/path/to/ndk',
-    'arm_dir': '/path/to/arm',
+    'icu_dir': '/path/to/arm',
     'toolchain_path': '/tmp/toolchain',
 }
 
